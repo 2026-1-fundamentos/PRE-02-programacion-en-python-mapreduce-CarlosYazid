8989f11a14330c28b14026157cda1fb88b4a112f
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2026-05-04 18:39:07 -0500

    Update word_count.py

--- homework/word_count.py
+++ homework/word_count.py
@@ -3,34 +3,61 @@
 # pylint: disable=broad-exception-raised
 
 import fileinput
+from collections import Counter
+import string
 import glob
-import os.path
+import os
 import time
 from itertools import groupby
 
 from toolz.itertoolz import concat, pluck
 
 
-
-def copy_raw_files_to_input_folder(n):
+def copy_raw_files_to_input_folder(n, input_path = "files/raw", output_path ="files/input"):
     """Generate n copies of the raw files in the input folder"""
 
+    for file in glob.glob(f"{input_path}/*"):
 
+        with open(file, "r", encoding="utf-8") as f:
+
+            text = f.read()
+
+            raw_filename_with_extension = os.path.basename(file)
+            raw_filename_without_extension = os.path.splitext(
+                raw_filename_with_extension
+            )[0]
+
+            for i in range(1, n+1):
+
+                new_filename = f"{raw_filename_without_extension}_{i}.txt"
+
+                with open(f"{output_path}/{new_filename}", "w", encoding="utf-8") as f2:
+                    f2.write(text)
 
 def load_input(input_directory):
     """Funcion load_input"""
+    sequences = []
+    files = glob.glob(f"{input_directory}/*")
+    for file in files:
+        with open(file, "r", encoding="utf-8") as f:
+            sequences += (file, f.readlines())
+    return sequences
 
+translator = str.maketrans("", "", string.punctuation)
 
-def preprocess_line(x):
-    """Preprocess the line x"""
+def preprocess_line(sequence):
+    """Preprocess the line"""
+    global translator
+    line = line.lower()
+    line = line.translate(translator)
+    return line.split()
 
-
-def map_line(x):
-    pass
+def map_line(line):
+    return Counter(preprocess_line(line))
 
 def mapper(sequence):
     """Mapper"""
-
+    for
 
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
@@ -46,6 +73,22 @@ def reducer(sequence):
 
 def create_directory(directory):
     """Create Output Directory"""
+
+    def clear_directory(directory):
+        if os.path.exists(directory):
+            for file in glob.glob(f"{directory}*"):
+                os.resume(file)
+    
+    def delete_directory(directory):
+        if os.path.exists(directory):
+            clear_directory(directory)
+            os.rmdir(directory)
+    
+    if os.path.exist(directory):
+        delete_directory(directory)
+    else:
+        os.makedirs(directory)
+
 
 
 def save_output(output_directory, sequence):
