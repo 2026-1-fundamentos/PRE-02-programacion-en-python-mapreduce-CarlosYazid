33c48877b4c3c2f72e27514efbaacb73cb0d01dd
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2026-05-06 15:30:56 -0500

    fix: fix errors

--- homework/word_count.py
+++ homework/word_count.py
@@ -2,15 +2,12 @@
 
 # pylint: disable=broad-exception-raised
 
-import fileinput
 from collections import Counter
 import string
 import glob
 import os
 import time
-from itertools import groupby
-
-from toolz.itertoolz import concat, pluck
+from functools import reduce
 
 
 def copy_raw_files_to_input_folder(n, input_path = "files/raw", output_path ="files/input"):
@@ -22,17 +19,17 @@ def copy_raw_files_to_input_folder(n, input_path = "files/raw", output_path ="fi
 
             text = f.read()
 
-            raw_filename_with_extension = os.path.basename(file)
-            raw_filename_without_extension = os.path.splitext(
-                raw_filename_with_extension
+        raw_filename_with_extension = os.path.basename(file)
+        raw_filename_without_extension = os.path.splitext(
+            raw_filename_with_extension
             )[0]
 
-            for i in range(1, n+1):
+        for i in range(1, n+1):
 
-                new_filename = f"{raw_filename_without_extension}_{i}.txt"
+            new_filename = f"{raw_filename_without_extension}_{i}.txt"
 
-                with open(f"{output_path}/{new_filename}", "w", encoding="utf-8") as f2:
-                    f2.write(text)
+            with open(f"{output_path}/{new_filename}", "w", encoding="utf-8") as f2:
+                f2.write(text)
 
 def load_input(input_directory):
     """Funcion load_input"""
@@ -40,12 +37,12 @@ def load_input(input_directory):
     files = glob.glob(f"{input_directory}/*")
     for file in files:
         with open(file, "r", encoding="utf-8") as f:
-            sequences += (file, f.readlines())
+            sequences += f.readlines()
     return sequences
 
 translator = str.maketrans("", "", string.punctuation)
 
-def preprocess_line(sequence):
+def preprocess_line(line):
     """Preprocess the line"""
     global translator
     line = line.lower()
@@ -57,19 +54,11 @@ def map_line(line):
 
 def mapper(sequence):
     """Mapper"""
-    for
-
-def shuffle_and_sort(sequence):
-    """Shuffle and Sort"""
-
-
-
-def compute_sum_by_group(group):
-    pass
-
-def reducer(sequence):
+    return map(map_line, sequence)
+    
+def reducer(mapped_sequence):
     """Reducer"""
-
+    return reduce(lambda x, y: x + y, mapped_sequence, Counter())
 
 def create_directory(directory):
     """Create Output Directory"""
@@ -77,49 +66,54 @@ def create_directory(directory):
     def clear_directory(directory):
         if os.path.exists(directory):
             for file in glob.glob(f"{directory}*"):
-                os.resume(file)
+                os.remove(file)
     
     def delete_directory(directory):
         if os.path.exists(directory):
             clear_directory(directory)
             os.rmdir(directory)
     
-    if os.path.exist(directory):
+    if os.path.exists(directory):
         delete_directory(directory)
     else:
         os.makedirs(directory)
 
 
 
-def save_output(output_directory, sequence):
+def save_output(output_directory = "files/output/", sequence = None):
     """Save Output"""
+    with open(os.path.join(output_directory, "part-00000"), "w", encoding="utf-8") as f:
+            for key, value in sequence.items():
+                f.write(f"{key}\t{value}\n")
 
 
-def create_marker(output_directory):
+def create_marker(output_directory = "files/output/"):
     """Create Marker"""
+    with open(os.path.join(output_directory, "_SUCCESS"), "w", encoding="utf-8") as f:
+            f.write("")
 
 
 def run_job(input_directory, output_directory):
     """Job"""
     sequence = load_input(input_directory)
     sequence = mapper(sequence)
-    sequence = shuffle_and_sort(sequence)
     sequence = reducer(sequence)
-    create_directory(output_directory)
     save_output(output_directory, sequence)
     create_marker(output_directory)
 
 
 if __name__ == "__main__":
 
+    INPUT_DIRECTORY = "files/input"
+    OUTPUT_DIRECTORY = "files/output"
+
+    create_directory(INPUT_DIRECTORY)
+    create_directory(OUTPUT_DIRECTORY)
     copy_raw_files_to_input_folder(n=1000)
 
     start_time = time.time()
 
-    run_job(
-        "files/input",
-        "files/output",
-    )
+    run_job(INPUT_DIRECTORY,OUTPUT_DIRECTORY)
 
     end_time = time.time()
     print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -10,28 +10,35 @@ import homework.word_count as wc
 def test_01():
     """Test Word Count"""
 
+    INPUT_DIRECTORY = "files/input"
+    OUTPUT_DIRECTORY = "files/output"
+
+    wc.create_directory(INPUT_DIRECTORY)
+    wc.create_directory(OUTPUT_DIRECTORY)
+    
+
     wc.copy_raw_files_to_input_folder(n=1000)
     wc.run_job(
-        "files/input",
-        "files/output",
+        INPUT_DIRECTORY,
+        OUTPUT_DIRECTORY,
     )
 
     #
     # Retorna error si la carpeta output/ no existe
-    if not os.path.exists("files/output/"):
+    if not os.path.exists(OUTPUT_DIRECTORY):
         raise Exception("Output directory does not exist")
 
     #
     # Retorna error si el archivo "_SUCCESS" no existe en la
     # carpeta output/
-    if not os.path.exists("files/output/_SUCCESS"):
+    if not os.path.exists(os.path.join(OUTPUT_DIRECTORY, "_SUCCESS")):
         raise Exception("Output directory is empty")
 
     #
     # Lee el contenido del archivo "part-00000" en la carpeta output/
-    # Cada linea en el archivo esta conformada por una clave un valor,
+    # Cada linea en el archivo esta conformada por una clave un value,
     # separados por un tabulador. Asigne pareja a un diccionario
-    with open("files/output/part-00000", "r", encoding="utf-8") as f:
+    with open(os.path.join(OUTPUT_DIRECTORY, "part-00000"), "r", encoding="utf-8") as f:
         lines = f.readlines()
         result = {}
         for line in lines:
